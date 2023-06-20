02164ba47ce143891ca9b696d79b32b4a6ea2cbd
Sara-Poyo-Pastor <txara88@hotmail.com>
2023-06-20 11:04:44 +0200

    fonts new

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,9 +1,10 @@
 import Footer from "../components/templates/footer/Footer";
+import Navbar from "../components/organisms/Navbar";
 
 export default function LandingPage() {
   return (
     <div>
-
+        <Navbar/>
         <Footer />
     </div>
   )
