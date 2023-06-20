5a6008bf08f29da9c131507602c6155ef5123068
anaencinasd <ana.encinasdiaz>
2023-06-20 12:33:25 +0200

    haciendo wash

--- src/components/templates/Block6_wash/Block6_wash.jsx
+++ src/components/templates/Block6_wash/Block6_wash.jsx
@@ -4,7 +4,7 @@ import "./block6_wash.css"
 export default function Block6_wash() {
   return (
     <div className="washContainer">
-      <TittleBlock text_block={Wash} />
+      <TittleBlock text_block={"Wash"} />
         
       
 
