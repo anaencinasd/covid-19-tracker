6e85d5f0d0f58d9a5adc48d5b3f4d19ab7c02656
anaencinasd <ana.encinasdiaz>
2023-06-20 11:58:58 +0200

    haciendo wash

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,10 +1,12 @@
 import Footer from "../components/templates/footer/Footer";
 import Navbar from "../components/organisms/Navbar";
+import Block6_wash from "../components/templates/Block6_wash/Block6_wash";
 
 export default function LandingPage() {
   return (
     <div>
         <Navbar/>
+        <Block6_wash />
         <Footer />
     </div>
   )
---
+++ src/components/templates/Block6_wash/Block6_wash.jsx
@@ -0,0 +1,9 @@
+import "./block6_wash.css"
+
+export default function Block6_wash() {
+  return (
+    <div className="washContainer">
+
+    </div>
+  )
+}
