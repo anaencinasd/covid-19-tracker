1d06c3e9972fe6eaa6de0365b1cf1a30e14a0422
anaencinasd <ana.encinasdiaz>
2023-06-20 14:05:21 +0200

    añadido wash + cambios en tipografía

--- src/components/templates/Block6_wash/Block6_wash.jsx
+++ src/components/templates/Block6_wash/Block6_wash.jsx
@@ -6,7 +6,7 @@ export default function Block6_wash() {
   return (
     <div className="washContainer">
       <TittleBlock text_block={"Wash"} content_block={"Wash your hands min 20 seconds"} />
-      <img src={wash} alt="" />
+      <img src={wash} alt="imágenes para lavar las manos" />
 
         
       
