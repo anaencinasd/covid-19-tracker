fcbe361974505ce4944d3c33d39eb892f1e31812
anaencinasd <ana.encinasdiaz>
2023-06-21 21:04:48 +0200

    landing con todos los bloques

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -2,7 +2,6 @@ import Footer from "../components/templates/footer/Footer";
 import Symptom from "../components/templates/block2_symptom/Symptom";
 import Prevent from "../components/templates/block3_prevent/prevent";
 import Block6_wash from "../components/templates/Block6_wash/Block6_wash";
-import Spreads from "../components/templates/block3_Spreads/Bloque_3_Spreads";
 import Bloque1 from "../components/templates/bloque1/Bloque1";
 import Bloque_3_Spreads from "../components/templates/block3_Spreads/Bloque_3_Spreads"
 import Block7_getApp from "../components/templates/block7_getApp/Block7_getApp";
@@ -16,8 +15,10 @@ return (
     <Bloque1/>
     <Symptom/>
     <Prevent/>
-    <Spreads/>
+    <Bloque_3_Spreads />
     <Block6_wash/>
+    <Block7_getApp />
+    <News />
     <Footer />
 
     </div>
