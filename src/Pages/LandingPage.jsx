b52ded88595bfda92f0554216a6f60f8e4b8af80
MaiteNavas <maitenavas626@gmail.com>
2023-06-21 09:31:47 +0200

    add block 2&3

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -4,30 +4,19 @@ import Navbar from "../components/organisms/Navbar";
 import Symptom from "../components/templates/block2_symptom/Symptom";
 import Prevent from "../components/templates/block3_prevent/prevent";
 import Block6_wash from "../components/templates/Block6_wash/Block6_wash";
-import cardSymptom from "../components/atoms/cardSymptom/CardSymptom";
-
-
-
-export default function LandingPage() {
-  return (
-    <div>
-    <Symptom/>
-    <Prevent/>
-
-
-      {/*<Navbar/>
-
-      <Footer />*/}
-=======
-//import Footer from "../components/templates/footer/Footer";
+import Footer from "../components/templates/footer/Footer";
 import Bloque1 from "../components/templates/bloque1/Bloque1";
+
 export default function LandingPage() {
   return (
     <div>
-        <Bloque1/>
-        
-        {/* <Footer /> */}
->>>>>>> features/navbar
+      <Navbar/>
+      <Bloque1/>
+      <Symptom/>
+      <Prevent/>
+      <Block6_wash/>
+      <Footer />
+
     </div>
   )
 }
