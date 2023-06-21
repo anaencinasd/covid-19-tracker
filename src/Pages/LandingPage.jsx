b8ffe0c01a4c24396713e1e04cea651c084aa889
anaencinasd <ana.encinasdiaz>
2023-06-21 09:52:55 +0200

    uniendo bloques en landing

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
+
 import Footer from "../components/templates/footer/Footer";
-import Navbar from "../components/organisms/Navbar";
 import Symptom from "../components/templates/block2_symptom/Symptom";
 import Prevent from "../components/templates/block3_prevent/prevent";
 import Block6_wash from "../components/templates/Block6_wash/Block6_wash";
@@ -10,7 +9,7 @@ import Bloque1 from "../components/templates/bloque1/Bloque1";
 export default function LandingPage() {
   return (
     <div>
-      <Navbar/>
+      
       <Bloque1/>
       <Symptom/>
       <Prevent/>
