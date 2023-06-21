9330b6638e3b58d0dc4252e339f906e0c156b156
anaencinasd <ana.encinasdiaz>
2023-06-21 10:21:02 +0200

    bloques unificados en landing page - falta tres bloques

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,30 +1,21 @@
-<<<<<<< HEAD
 import Footer from "../components/templates/footer/Footer";
-import Navbar from "../components/organisms/Navbar";
 import Symptom from "../components/templates/block2_symptom/Symptom";
 import Prevent from "../components/templates/block3_prevent/prevent";
 import Block6_wash from "../components/templates/Block6_wash/Block6_wash";
+import Bloque1 from "../components/templates/bloque1/Bloque1"
+import Bloque_3_Spreads from "../components/templates/block3_Spreads/Bloque_3_Spreads"
 
 
 
 export default function LandingPage() {
   return (
     <div>
-  
-        <Navbar/>
-        <Symptom />
-        {<Block6_wash />}
-        <Footer />
-=======
-//import Footer from "../components/templates/footer/Footer";
-import Bloque1 from "../components/templates/bloque1/Bloque1";
-export default function LandingPage() {
-  return (
-    <div>
-        <Bloque1/>
-        
-        {/* <Footer /> */}
->>>>>>> features/navbar
+      <Bloque1 />
+      <Symptom />
+      <Prevent />
+      <Bloque_3_Spreads />
+      <Block6_wash />
+      <Footer /> 
     </div>
   )
 }
