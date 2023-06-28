72dcdb53e0a2010d09ad8a703c98f6d8b4b5acd5
Sara-Poyo-Pastor <txara88@hotmail.com>
2023-06-28 11:02:08 +0200

    progress

--- src/Pages/CountryInfo.jsx
+++ src/Pages/CountryInfo.jsx
@@ -1,9 +1,14 @@
 import Tracker from "../components/organisms/Tracker"
+import Navbar_tracker from "../components/molecules/navbar_tracker/Navbar_tracker"
+
 
 export default function CountryInfo() {
   return (
     <>
-      <Tracker />
+      <div className="tracker_container_principal">
+        <Navbar_tracker/>
+        <Tracker/>
+      </div>
     </>
   )
 }
