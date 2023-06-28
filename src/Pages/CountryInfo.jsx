6742d5a06da3c132255e73714a152b06ba0fafa9
NINETTHE <raissa.escobar1408@gmail.com>
2023-06-28 11:41:44 +0200

    Atomo CardTotalTracker y MoleculeTotalTracker terminados en estilos

--- src/Pages/CountryInfo.jsx
+++ src/Pages/CountryInfo.jsx
@@ -1,9 +1,12 @@
 import Tracker from "../components/organisms/Tracker"
+import CardAtomTotalTrack from "../components/atoms/cardTotal/CardAtomTotalTracker"
+import CardMoleculeTotalTracker from "../components/molecules/cardMoleculeTotalTracker/CardMoleculeTotalTracker";
 
 export default function CountryInfo() {
   return (
     <>
       <Tracker />
+      <CardMoleculeTotalTracker/>
     </>
   )
 }
