cbedb8d96c01270ef6656736f44af577aeb1da3c
NINETTHE <raissa.escobar1408@gmail.com>
2023-06-30 11:00:04 +0200

    Modificación de css en CardsTotal

--- src/Pages/CountryInfo.jsx
+++ src/Pages/CountryInfo.jsx
@@ -1,5 +1,6 @@
 import Tracker from "../components/organisms/Tracker"
 import Navbar_tracker from "../components/molecules/navbar_tracker/Navbar_tracker"
+import CardMoleculeTotalTracker from "../components/molecules/cardMoleculeTotalTracker/CardMoleculeTotalTracker"
 
 
 export default function CountryInfo() {
@@ -8,6 +9,7 @@ export default function CountryInfo() {
       <div className="tracker_container_principal">
         <Navbar_tracker/>
         <Tracker/>
+        <CardMoleculeTotalTracker/>
       </div>
     </>
   )
