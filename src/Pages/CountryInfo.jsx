ca5ead6489433c25bf4febcc527a5bdcc9254a0e
anaencinasd <ana.encinasdiaz>
2023-07-03 15:40:30 +0200

    css y navbar revisados y editados

--- src/Pages/CountryInfo.jsx
+++ src/Pages/CountryInfo.jsx
@@ -1,18 +1,17 @@
-import Tracker from "../components/organisms/Tracker"
-import Navbar_tracker from "../components/molecules/navbar_tracker/Navbar_tracker"
-import Dropdown from "../components/atoms/dropdown/Dropdown"
-
+import Tracker from "../components/organisms/Tracker";
+import Navbar_tracker from "../components/molecules/navbar_tracker/Navbar_tracker";
+import Dropdown from "../components/atoms/dropdown/Dropdown";
 
 export default function CountryInfo() {
   return (
     <>
+      <Navbar_tracker />
       <div className="tracker_container_principal">
-        <Navbar_tracker/>
-        <Tracker/>
+        <Tracker />
         <div className="dropdownCards">
           <Dropdown />
         </div>
       </div>
     </>
-  )
+  );
 }
--- src/Pages/CountryTopTen.jsx
+++ src/Pages/CountryTopTen.jsx
@@ -1,17 +1,17 @@
-import Tracker from "../components/organisms/Tracker"
-import Navbar_tracker from "../components/molecules/navbar_tracker/Navbar_tracker"
-import Top10CasesMolecule from "../components/molecules/top10CasesMolecule/Top10CasesMolecule"
+import Tracker from "../components/organisms/Tracker";
+import Navbar_tracker from "../components/molecules/navbar_tracker/Navbar_tracker";
+import Top10CasesMolecule from "../components/molecules/top10CasesMolecule/Top10CasesMolecule";
 
 export default function CountryTopTen() {
   return (
     <>
-      <div className="tracker_container_principal">
-        <Navbar_tracker/>
-        <Tracker/>
-        <Top10CasesMolecule/>
-
-
+    <div>
+    <Navbar_tracker />
+    </div>  
+    <div className="tracker_container_principal">
+        <Tracker />
+        <Top10CasesMolecule />
       </div>
     </>
-  )
+  );
 }
