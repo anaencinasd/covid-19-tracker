e3babbb013e7fc78bc4941029feda12c00ca40c2
MaiteNavas <maitenavas626@gmail.com>
2023-07-03 14:27:35 +0200

    responsive countryTopTen

--- src/Pages/CountryTopTen.jsx
+++ src/Pages/CountryTopTen.jsx
@@ -1,12 +1,13 @@
 import Tracker from "../components/organisms/Tracker"
 import Navbar_tracker from "../components/molecules/navbar_tracker/Navbar_tracker"
 import Top10CasesMolecule from "../components/molecules/top10CasesMolecule/Top10CasesMolecule"
-
+import './countryTopTen.css'
 export default function CountryTopTen() {
   return (
     <>
-      <div className="tracker_container_principal">
+      
         <Navbar_tracker/>
+        <div className="tracker_container_principal">
         <Tracker/>
         <Top10CasesMolecule/>
 
