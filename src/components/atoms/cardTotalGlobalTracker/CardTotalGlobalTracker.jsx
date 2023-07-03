6faedbb889390feb70c16f6cb221b32a229e8ed0
NINETTHE <raissa.escobar1408@gmail.com>
2023-07-03 20:04:16 +0200

    cardsGlobalTerminadas

--- src/components/atoms/cardTotalGlobalTracker/CardTotalGlobalTracker.jsx
+++ src/components/atoms/cardTotalGlobalTracker/CardTotalGlobalTracker.jsx
@@ -1,13 +1,25 @@
 import "./cardTotalGlobalTracker.css"
+import useFetch from "../../../service/useFetch";
+import DynamicUrl from "../../../service/DynamicUrl";
+import { useEffect, useState } from "react";
 
 export default function CardTotalGlobalTracker(props) {
+
+    const { data } = useFetch("all", DynamicUrl);
+    const [globalData, setGlobalData] = useState(null);
+
+    useEffect(() => {
+        if (data) {
+        setGlobalData(data);
+    }
+}, [data]);
 return (
     
         <div className="cardTotalGlobalTracker2">
             <img className="imgMolecule" src={props.imgCardMoleculeGlobalTracker}/>
             <div className="containerTotalGlobalTracker2">
                 <p className="styleParagraphTotalGlobal">{props.textParagraphTotalGlobal}</p>
-                <h5 className="styleTitleGlobalTracker">{props.textContentGlobalTracker}</h5>
+                <h4 className="styleTotalGlobalTracker2"> {globalData != null ? ( `${globalData[props.info]}`) : ("Loading data")}</h4>
             </div>
         </div>
 )
--- src/components/molecules/cardMoleculeGlobalTracker/CardMoleculeGlobalTracker.jsx
+++ src/components/molecules/cardMoleculeGlobalTracker/CardMoleculeGlobalTracker.jsx
@@ -3,16 +3,39 @@ import iconDefault from "../../../assets/images/icons/covid-blue.svg";
 import iconGreen from "../../../assets/images/icons/covid-green.svg";
 import iconRed from "../../../assets/images/icons/covid-red.svg";
 import iconRedark from "../../../assets/images/icons/covid-redark.svg";
-import iconCall from "../../../assets/images/icons/telephone.svg"
+import iconCall from "../../../assets/images/icons/telephone.svg";
+import "./cardMoleculeGlobalTracker.css";
 
 export default function CardMoleculeGlobalTracker() {
 return (
     <div className="containerCardsGlobalTracker2">
-    <CardTotalGlobalTracker imgCardMoleculeGlobalTracker={iconDefault} textParagraphTotalGlobal="Total Confirmed" textContentGlobalTracker="1234567890"/>
-    <CardTotalGlobalTracker imgCardMoleculeGlobalTracker={iconGreen} textParagraphTotalGlobal="Total Recovered" textContentGlobalTracker=""/>
-    <CardTotalGlobalTracker imgCardMoleculeGlobalTracker={iconRed} textParagraphTotalGlobal="Total Deaths" textContentGlobalTracker=""/>
-    <CardTotalGlobalTracker imgCardMoleculeGlobalTracker={iconRedark} textParagraphTotalGlobal=" New Deaths" textContentGlobalTracker=""/>
-    <CardTotalGlobalTracker imgCardMoleculeGlobalTracker={iconCall} textParagraphTotalGlobal="Help Line No." textContentGlobalTracker="198"/>
+    <CardTotalGlobalTracker 
+    imgCardMoleculeGlobalTracker={iconDefault} 
+    textParagraphTotalGlobal="Total Confirmed" 
+    info="cases"/>
+
+    <CardTotalGlobalTracker 
+    imgCardMoleculeGlobalTracker={iconGreen} 
+    textParagraphTotalGlobal="Total Recovered" 
+    info="recovered"/>
+
+    <CardTotalGlobalTracker 
+    imgCardMoleculeGlobalTracker={iconRed} 
+    textParagraphTotalGlobal="Total Deaths" 
+    info="deaths"/>
+
+    <CardTotalGlobalTracker 
+    imgCardMoleculeGlobalTracker={iconRedark} 
+    textParagraphTotalGlobal=" New Deaths" 
+    info="todayDeaths"/>
+    
+    <div className="help"> 
+        <img  className="iconHelp" src={iconCall} />
+        <div className="paragraphAndNumber">
+            <p className="paragraphHelp">Help Line No.</p>
+            <h4 className="styleTotalGlobalTracker2">198</h4>
+        </div>
+    </div>
 
     </div>
 )
